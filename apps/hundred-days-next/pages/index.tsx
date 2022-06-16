6efe5bb5240ac094872d26337211b634b66fe28f
xujianxiang <584972029@qq.com>
2022-06-16 12:01:18 +0800

    fix: change url

--- apps/hundred-days-next/pages/index.tsx
+++ apps/hundred-days-next/pages/index.tsx
@@ -9,7 +9,7 @@ export function Index() {
         nextProjects.map((x, i) => {
           return (
             <li key={x.name} className="hover:text-blue-700 cursor-pointer">
-              <Link href={process.env.NODE_ENV === 'development' ? `/${x.link}` : `/${x.tag}/${x.link}`}>
+              <Link href={`/${x.link}`}>
                 <span>
                   {computedIndex(i)}-{x.name}
                 </span>
@@ -20,7 +20,7 @@ export function Index() {
       }
 
     </ul>
-  </Layout>
+  </Layout >
 };
 
 export default Index;
